035b56bc5edc4b4fe14bad512c7161a8663a8e88
Anas jaidi <anas.jaidi@icloud.com>
2023-06-27 07:23:49 +0100

    add: basic app router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { Route } from 'react-router-dom'
+import homePage from './pages/home/home.page'
+import chatPage from './pages/chat/chat.page'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello, World!
+      <Route path='/' exact component={homePage}></Route>
+      <Route path='/chats' component={chatPage}></Route>
     </>
   )
 }
