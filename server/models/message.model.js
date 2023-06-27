82102a6b008a1f7b7168868f20ab4ca613f5486b
Anas jaidi <anas.jaidi@icloud.com>
2023-06-27 07:24:51 +0100

    add: message model

---
+++ server/models/message.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+  {
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", chatModel);
+
+module.exports.Message = Message;
