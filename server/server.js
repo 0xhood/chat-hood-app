e57adcd8090511a446ed1cc5e730b9d9fabf88ef
Anas jaidi <anas.jaidi@icloud.com>
2023-06-27 07:25:06 +0100

    update: server to config cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { chats } = require('./data/data')
 const { config } = require('dotenv')
+const cors = require('cors');
+
+
 
 config();
 
@@ -8,6 +11,7 @@ config();
 const app = express()
 const port = process.env.PORT
 
+app.use(cors({origin: '*'}))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/api/v1/chats', (req, res) => {
@@ -15,8 +19,9 @@ app.get('/api/v1/chats', (req, res) => {
 })
 
 app.get('/api/v1/chats/:id', (req, res) => {
+    console.log('anas')
     res.json(chats.find(chat => chat._id === req.params.id));
 })
 
 
-app.listen(port, () => console.log(`app listening on port ${port}!`)) 
+app.listen(port, () => console.log(`app listening on port ${port}`)) 
