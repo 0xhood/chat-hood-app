38aff231a19cf8954e0c42d047c8bedd51ec4f98
Anas jaidi <anas.jaidi@icloud.com>
2023-06-27 06:43:05 +0100

    add: env vars support

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const { chats } = require('./data/data')
+const { config } = require('dotenv')
 
+config();
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/api/v1/chats', (req, res) => {
     res.json(chats);
+})
+
+app.get('/api/v1/chats/:id', (req, res) => {
+    res.json(chats.find(chat => chat._id === req.params.id));
 })
 
 
